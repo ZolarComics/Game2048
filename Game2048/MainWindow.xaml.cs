4bf51de07fc1949779e0808d52c9406e2af20905
serkho zoliar <srgjzln@gmail.com>
2021-06-08 20:34:55 +1000

    Добавьте файлы проекта.

---
+++ Game2048/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game2048
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int score;
+        Label[,] labels = new Label[4, 4];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            score = 0;
+            Score.Text = score.ToString();
+            
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up: GoUp(); break;
+                case Key.Down: GpDown(); break;
+                case Key.Left: GoLeft(); break;
+                case Key.Right: GoRight(); break;
+                default: break;
+            }
+        }
+
+        private void GoUp()
+        {
+            score = 2048;
+            Score.Text = score.ToString();
+        }
+
+        private void GpDown()
+        {
+            throw new NotImplementedException();
+        }
+        private void GoLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GoRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+        }
+    }
+}
---
+++ Game2048/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Game2048
+{
+    class Model
+    {
+        private static SolidColorBrush StaleFor2 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor4 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor8 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor16 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor32 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor64 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor128 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor256 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor512 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor1024 = new SolidColorBrush();
+        private static SolidColorBrush StaleFor2048 = new SolidColorBrush();
+
+        private static SolidColorBrush Empty = new SolidColorBrush();
+    }
+}
